6ab6c09875ec77af4010c9a496ae8201a6cd5480
blueskii <blueskii@naver.com>
2022-11-15 09:14:36 +0900

    실행1 추가

--- src/App.java
+++ src/App.java
@@ -4,8 +4,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("애플리케이션 실행");
 		System.out.println("실행1");
-		System.out.println("실행2");
-		System.out.println("실행3");
 	}
 
 }
